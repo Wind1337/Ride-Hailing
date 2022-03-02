3642c0481a4c417cf67f9681fcab2782179f4246
Carlsen Teo <carlsen@inspirous.us>
2022-03-02 23:07:48 +0800

    Add LinkedList ADTs for Driver and Passenger

---
+++ Driver.py
@@ -0,0 +1,57 @@
+class DriverNode:
+    def __init__(self, carType, seatCapacity, location):
+        self.carType = carType
+        self.seatCapacity = seatCapacity
+        self.location = location
+        self.next = None
+        self.prev = None
+
+
+class DriverLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insertAtHead(self, node):
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.head.prev = node
+            node.next = self.head
+            self.head = node
+
+    def insertAtTail(self, node):
+        if self.tail is None:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+
+    def search(self, value):
+        temp = self.head
+        while temp is not None:
+            if temp.data == value:
+                return temp
+            temp = temp.next
+        print("Not Found")
+
+    def delete(self, value):
+        temp = self.head
+        while temp is not None:
+            if temp.data != value:
+                temp = temp.next
+            else:
+                if temp == self.head:
+                    self.head = self.head.next
+                    self.head.prev = None
+                elif temp == self.tail:
+                    self.tail = self.tail.prev
+                    self.tail.next = None
+                else:
+                    prev = temp.prev
+                    succ = temp.next
+                    prev.next = succ
+                    succ.prev = prev
+                del temp
+        print("Not Found")
