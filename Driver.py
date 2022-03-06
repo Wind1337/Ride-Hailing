f7282d45c5385273c46dab0a87a662b4e63d8ef8
Carlsen Teo <carlsen@inspirous.us>
2022-03-06 17:14:57 +0800

    Matching: Prototype concept level matching?

--- Driver.py
+++ Driver.py
@@ -1,5 +1,10 @@
 class DriverNode:
-    def __init__(self, carType, seatCapacity, location):
+    driverID = 0
+
+    def __init__(self, name, carType, seatCapacity, location):
+        DriverNode.driverID += 1
+        self.driverID = DriverNode.driverID
+        self.name = name
         self.carType = carType
         self.seatCapacity = seatCapacity
         self.location = location
@@ -11,6 +16,9 @@ class DriverLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def getHead(self):
+        return self.head
 
     def insertAtHead(self, node):
         if self.head is None:
@@ -28,30 +36,31 @@ class DriverLinkedList:
             node.prev = self.tail
             self.tail = node
 
-    def search(self, value):
+    def search(self, inputID):
         temp = self.head
         while temp is not None:
-            if temp.data == value:
+            if temp.driverID == inputID:
                 return temp
             temp = temp.next
         print("Not Found")
 
-    def delete(self, value):
+    def delete(self, inputID):
         temp = self.head
         while temp is not None:
-            if temp.data != value:
+            if temp.driverID != inputID:
                 temp = temp.next
             else:
                 if temp == self.head:
                     self.head = self.head.next
-                    self.head.prev = None
+                    #self.head.prev = None
                 elif temp == self.tail:
                     self.tail = self.tail.prev
-                    self.tail.next = None
+                    #self.tail.next = None
                 else:
                     prev = temp.prev
                     succ = temp.next
                     prev.next = succ
                     succ.prev = prev
                 del temp
+                return
         print("Not Found")
